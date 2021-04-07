2c90eab4d1857e5b7ba3c11b53d519f5e7a6986e
gucheen <guchengf@gmail.com>
2021-04-07 17:36:04 +0800

    fix: 值得买数据

--- smzdm.js
+++ smzdm.js
@@ -6,7 +6,7 @@ const data = await alfy.fetch(`https://www.smzdm.com/homepage/json_more?timesort
 
 const items = data.data
 	.map(item => {
-    if (item.article_type === '好价') {
+    if (item.yh_type === 'youhui') {
       return {
         title: item.article_title,
         subtitle: `${item.article_mall} - ${item.article_price} - ${item.article_worthy}⬆ - ${item.article_unworthy}⬇ - ${item.article_comment}评`,
