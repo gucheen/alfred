10bebf3b6e15a371a505d1624ef719092ca5b13f
gucheen <guchengf@gmail.com>
2022-09-26 17:38:18 +0800

    fix: scripts

--- smzdm.js
+++ smzdm.js
@@ -1,24 +1,30 @@
-const alfy = require('alfy');
+import alfy from 'alfy'
 
-const time = Math.floor(Date.now() / 1000);
+const time = Math.floor(Date.now() / 100)
 
-const data = await alfy.fetch(`https://www.smzdm.com/homepage/json_more?timesort=${time}&p=1&past_num=20`);
+try {
+  const data = await alfy.fetch(`https://www.smzdm.com/homepage/json_more?timesort=${time}&p=1&past_num=20`)
 
-const items = data.data
+  const items = data.data
 	.map(item => {
     if (item.yh_type === 'youhui') {
       return {
+        uid: item.article_id,
         title: item.article_title,
         subtitle: `${item.article_mall} - ${item.article_price} - ${item.article_worthy}⬆ - ${item.article_unworthy}⬇ - ${item.article_comment}评`,
         arg: item.article_url,
       }
     } else {
       return {
+        uid: item.article_id,
         title: item.article_title,
         subtitle: item.article_content,
         arg: item.article_url,
       }
     }
-  });
+  })
 
-alfy.output(items);
+  alfy.output(items)
+} catch (error) {
+  alfy.log(error)
+}
